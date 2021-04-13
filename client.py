604892b61b8b8686b2e528bca23db2caf3cd38ec
Clint <55072157+clintonbf@users.noreply.github.com>
2021-04-12 18:05:23 -0700

    bugfix: packets sent correctly

--- client.py
+++ client.py
@@ -229,11 +229,8 @@ def take_turn(game_data: GameData_a4, s: socket):
         context = REQ_CONTEXTS.MAKE_MOVE.value
         payload = int(proposed_play)
 
-    # send_packet_one(s, game_data.get_uid(), action, context, 1, payload)  # Janky-ass
-    # send_packet_two(s, game_data.get_uid(), action, context, 1, payload)  # Padding cheat
-    # send_packet_three(s, game_data.get_uid(), action, context, 1, payload)  # New hotness?
-    # send_packet_four(s, game_data.get_uid(), action, context, 1, payload)  # New new hotness?
-    send_packet_five(s, game_data.get_uid(), action, context, 1, payload)  # New new new hotness?
+    packet = pack("!LBBBB", game_data.get_uid(), action, context, 1, payload)
+    s.sendall(packet)
 
     # Now get confirmation from Server
     play_response = get_message(s)
@@ -250,100 +247,6 @@ def take_turn(game_data: GameData_a4, s: socket):
     else:  # TODO Handle errors
         print(RESPONSE_MESSAGES[response_status])
         return False
-
-
-def send_packet_one(s: socket, uid: int, action: int, context: int, payload_length: int, payload: int):
-    """
-    Sends a packet using "combine" methodology
-    :return:
-    """
-    packet_part_1 = uid.to_bytes(4, 'little')
-    packet_part_2 = bytearray([action, context, payload_length, payload])
-
-    packet = bytearray(packet_part_1)
-    packet.extend(packet_part_2)
-
-    s.send(packet)  # Sending a payload on a quit. Technically there shouldn't be one
-
-
-def send_packet_two(s: socket, uid: int, action: int, context: int, payload_length: int, payload: int):
-    """
-    Sends a packet using the "cheating padding" methodology
-    :param s:
-    :param uid:
-    :param action:
-    :param context:
-    :param payload_length:
-    :param payload:
-    :return:
-    """
-
-    packet = [0, 0, 0, uid, action, context, payload_length, payload]
-    s.sendall(bytes(packet))
-
-
-def send_packet_three(s: socket, uid: int, action: int, context: int, payload_length: int, payload: int):
-    """
-    Sends a packet using the "two-step" method.
-    :param s:
-    :param uid:
-    :param action:
-    :param context:
-    :param payload_length:
-    :param payload:
-    :return:
-    """
-    packet_part_1 = uid.to_bytes(4, 'little')
-    s.sendall(packet_part_1)
-
-    packet_part_2 = [action, context, payload_length, payload]
-    s.sendall(bytes(packet_part_2))
-
-
-def send_packet_four(s: socket, uid: int, action: int, context: int, payload_length: int, payload: int):
-    """
-    Sends a packet using the "two-step" method.
-    :param s:
-    :param uid:
-    :param action:
-    :param context:
-    :param payload_length:
-    :param payload:
-    :return:
-    """
-    packet_part_1 = uid.to_bytes(4, 'little')
-    s.sendall(packet_part_1)
-
-    packet_part_2 = [action, context, payload_length, payload]
-    for i in range(0, len(packet_part_2)):
-        s.sendall(packet_part_2[i].to_bytes(1, 'big'))
-
-
-def send_packet_five(s: socket, uid: int, action: int, context: int, payload_length: int, payload: int):
-    x = pack("!lhhhh", uid, action, context, payload_length, payload)
-
-    s.sendall(x)
-
-
-def send_packet(s: socket, packet: [int]):  # TODO Delete
-    f"""
-
-    :param s: {socket} TCP socket
-    :param packet: {list} data to send
-    :return: {None}
-    """
-    # Packet is composed of
-    #   uuid (4 bytes)                  [0]
-    #   status (1 byte)                 [1]
-    #   context (1 byte)                [2]
-    #   payload length (1 byte)         [3] # TTT moves are always 1 byte long. A convenient shortcut
-    #   payload (payload_length bytes)  [4]
-
-    s.sendall(packet[0].to_bytes(4, 'big'))
-
-    for i in range(1, len(packet)):
-        s.sendall(packet[i].to_bytes(1, 'big'))
-
 
 def handshake(s: socket) -> int:
     f"""
